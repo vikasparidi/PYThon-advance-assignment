1962eb880161f36c05b9a6ed486dc4b89c6cd3f4
Paridi Vikas <71751311+vikasparidi@users.noreply.github.com>
2022-05-23 19:54:34 +0530

    Add files via upload

---
+++ python advance assignment1.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Q1. What is the purpose of Python's OOP?
+
+# The main thing that comes into picture when we tak about OOPS concept is all about the CLASS and OBJECTS.
+# 
+# The main purpose to use this class and object is to get the reusability of code under a single class.
+# 
+# 
+# The main aim is to implement real entitites like inheritance,polymorphism,encapsulation.
+
+# # Q2. Where does an inheritance search look for an attribute?
+
+# Inheritance is the procress in which one class inherits the properties of the other class.
+# 
+# 
+# 
+# Mainly the properties are called attributes.Here the transfer of attributes takes from a parent class .And the class which inherit the attributes are called child class
+
+# # Q3. How do you distinguish between a class object and an instance object?
+
+# MAinly the class is an blueprint tht is made for the further involvment with the code.Where as the instance object is the one that is real and being used as a class object.
+
+# # Q4. What makes the first argument in a class’s method function special?
+
+# # The first argument of the class including the init method shows that it is the first instance of the class.And from here the inherite properties transfers to the child classes.
+
+# # Q5. What is the purpose of the __init__ method?
+
+# The main purpose of init method is that it creates a pointer with respect to every element that is being passed.
+# 
+# 
+# init usually represents as __init(self,"a","b")
+# 
+# 
+# here self is the pointer where it point outs the every element
+
+# # Q6. What is the process for creating a class instance?
+
+# Create a class.....with using the keyword called class
+# 
+# 
+# call the class using a class name and assign it to a variable.
+# 
+# 
+# 
+# pass what ever arguments required using __init__
+# 
+# 
+# 
+
+# # Q7. What is the process for creating a class?
+
+# The process of creating a class starts with the 
+# 
+# 1) create a class using a keyword called class. Give suitable name to the class.
+# 
+# 
+# 
+
+# # Q8. How would you define the superclasses of a class?
+
+# The class from which the class inherits is called a superclass or parentclass
+
+# In[ ]:
+
+
+
+
